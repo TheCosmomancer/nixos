1848f298ac251c1dd98447c0563fd13a63570d14
TheCosmomancer <lecosmomancer@gmail.com>
2025-03-25 17:24:10 +0330

    adding hyperland

--- configuration.nix
+++ configuration.nix
@@ -127,10 +127,7 @@
     WLR_NO_HARDWARE_CURSORS = "1";
     NIXOS_OZONE_WL = "1";
   };
-  hardware= {
-    opengl.enable = true;
-    nvidia.modesetting.enable = true;
-  };
+  hardware.opengl.enable = true;
   xdg.portal = {
     enable = true;
     extraPortals = [pkgs.xdg-desktop-portal];
