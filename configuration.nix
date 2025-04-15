61b65a7bbfde24697ca2e1880971877cff32d6b1
cosmomancer <lecosmomancer@gmail.com>
2025-04-15 13:33:05 +0330

    .

--- configuration.nix
+++ configuration.nix
@@ -1,14 +1,10 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, inputs, ... }:
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
       ./hardware-configuration.nix
-        inputs.home-manager.nixosModules.default
+      inputs.home-manager.nixosModules.default
     ];
 
   # Bootloader.
@@ -22,6 +18,10 @@
 			device = "nodev";
 		};
 	};
+  distro-grub-themes = {
+    enable = true;
+    theme = "nixos";
+  };
   
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
@@ -29,11 +29,9 @@
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
+  
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
   time.timeZone = "Asia/Tehran";
 
   # Select internationalisation properties.
@@ -44,13 +42,14 @@
   services.xserver.enable = true;
 
   # Enable the hyprland.
-  services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.displayManager.gdm = {
+    enable = true;
+    wayland = true;
+  };
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
   };
-
-  services.logind.lidSwitch = "ignore";
   
   # Configure keymap in X11
   services.xserver.xkb = {
@@ -60,7 +59,7 @@
   };
 
   # Enable CUPS to print documents.
-  services.logind.extraConfig = "HandleLidSwitch=ignore";
+  services.printing.enable = true;
 
   # Enable sound with pipewire.
   services.pulseaudio.enable = false;
@@ -70,21 +69,22 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
     jack.enable = true;
-
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
     #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+  # Enable touchpad support and disable the lid closure switch for useage 
+  #while connected to a montitor.
+  services.xserver.libinput.enable = true;
+  services.logind.lidSwitch = "ignore";
+  services.logind.extraConfig = "HandleLidSwitch=ignore";
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.cosmomancer = {
     isNormalUser = true;
-    description = "Cosmomancer";
+    description = "cosmomancer";
     extraGroups = [ "networkmanager" "wheel" "input" ];
     shell = pkgs.zsh;
     packages = with pkgs; [
@@ -95,7 +95,11 @@
     ];
   };
 
-  security.sudo.wheelNeedsPassword = false;
+  security.sudo.extraRules = [
+    { users = [ "cosmomancer" ];
+      options = [ "NOPASSWD" ];
+    }
+  ];
 
   home-manager = {
     extraSpecialArgs = {inherit inputs; };
@@ -109,12 +113,10 @@
   nixpkgs.config.allowUnfree = true;
   #allow flakes
   nix.settings.experimental-features = ["nix-command" "flakes"];
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #hyprland
     hyprpolkitagent
+    polkit_gnome
     hyprpaper
     ghostty
     rofi-wayland
@@ -126,7 +128,7 @@
     xfce.thunar-volman
     hyprland-qtutils
     #hyprpanel
-    #hyprpanel
+    hyprpanel
     wireplumber
     libgtop
     networkmanagerapplet
@@ -156,35 +158,41 @@
     nomacs
     bottles
     peazip
+    wget
+    # List packages installed in system profile. To search, run:
+    # $ nix search wget
+
     #fun &flair
     fastfetch
     cmatrix
     cbonsai
     nwg-look
+    libsForQt5.qt5ct
     dracula-icon-theme
     lavanda-gtk-theme
     bibata-cursors
+    sddm-sugar-dark
     #nerd-fonts.jetbrains-mono MAKE SURE to manually install
-  ]; # rofi, hyprpaper, vscode and firefox anve some nice home-manager support
+  ]; # TODO firefox home-manager
 
   programs.zsh.enable = true;
   services.upower.enable = true;
   services.power-profiles-daemon.enable = true;
-  #services.tlp.enable = true;
   services.thermald.enable = true;
 
   environment.sessionVariables ={
     WLR_NO_HARDWARE_CURSORS = "1";
     NIXOS_OZONE_WL = "1";
+    QT_QPA_PLATFORMTHEME = "qt5ct";
   };
   xdg.portal = {
     enable = true;
     extraPortals = [pkgs.xdg-desktop-portal-hyprland];
   };
   nix.gc = {
-    automatic = true;  # Enable the automatic garbage collector
-    dates = "weekly";   # When to run the garbage collector
-    options = "--delete-older-than 14d";    # Arguments to pass to nix-collect-garbage
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 14d";
   };
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
@@ -193,8 +201,6 @@
   #   enable = true;
   #   enableSSHSupport = true;
   # };
-
-  # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
@@ -207,19 +213,10 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
+  #just don't change this and you'll be fine
+  system.stateVersion = "24.11";
 
-
-
-
-
-
+  # NVIDIA:
 
   # Install nvidia drivers
   hardware.graphics = {
@@ -227,22 +224,22 @@
   };
 
   # Load nvidia driver for Xorg and Wayland
-#  services.xserver.videoDrivers = ["nvidia"];
+  services.xserver.videoDrivers = ["nvidia"];
 
-#  hardware.nvidia = {
+  hardware.nvidia = {
 
     # Modesetting is required.
-#    modesetting.enable = true;
+    modesetting.enable = true;
 
     # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
     # Enable this if you have graphical corruption issues or application crashes after waking
     # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
     # of just the bare essentials.
-#    powerManagement.enable = false;
+    powerManagement.enable = false;
 
     # Fine-grained power management. Turns off GPU when not in use.
     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-#    powerManagement.finegrained = true;
+    powerManagement.finegrained = true;
 
     # Use the NVidia open source kernel module (not to be confused with the
     # independent third-party "nouveau" open source driver).
@@ -250,23 +247,23 @@
     # supported GPUs is at: 
     # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
     # Only available from driver 515.43.04+
-#    open = false;
-#
-#    # Enable the Nvidia settings menu,
-#	# accessible via `nvidia-settings`.
-#    nvidiaSettings = true;
-#
-#    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-#    package = config.boot.kernelPackages.nvidiaPackages.stable;
-#  };
-#  hardware.nvidia.prime = {
-#    reverseSync.enable = true;
-#    # Enable if using an external GPU
-#    allowExternalGpu = false;
-#		# Make sure to use the correct Bus ID values for your system!
-#		intelBusId = "PCI:0:1:0";
-#		nvidiaBusId = "PCI:0:6:0";
-#                # amdgpuBusId = "PCI:54:0:0"; For AMD GPU
-#	};
-#  hardware.graphics.enable32Bit = true;
+    open = false;
+
+    # Enable the Nvidia settings menu,
+	  # accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    };
+    hardware.nvidia.prime = {
+      reverseSync.enable = true;
+      # Enable if using an external GPU
+      allowExternalGpu = false;
+      # Make sure to use the correct Bus ID values for your system!
+      intelBusId = "PCI:0:1:0";
+      nvidiaBusId = "PCI:0:6:0";
+      # amdgpuBusId = "PCI:54:0:0"; For AMD GPU
+	};
+  hardware.graphics.enable32Bit = true;
 }
